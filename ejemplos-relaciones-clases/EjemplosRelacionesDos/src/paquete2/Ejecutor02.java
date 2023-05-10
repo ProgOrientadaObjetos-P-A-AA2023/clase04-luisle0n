557757093de74a8d8388230a49827ba0acd9c418
luisle0n <lfleon9@utpl.edu.ec>
2023-05-10 12:54:23 -0500

    Ejemploo04

--- ejemplos-relaciones-clases/EjemplosRelacionesDos/src/paquete2/Ejecutor02.java
+++ ejemplos-relaciones-clases/EjemplosRelacionesDos/src/paquete2/Ejecutor02.java
@@ -10,12 +10,42 @@ package paquete2;
  * @author reroes
  */
 public class Ejecutor02 {
-    
+
     public static void main(String[] args) {
         // 1. Crear una persona de Barrio el Valle de la ciudad de Loja
         // 2. Crear una persona del Barrio Central de la ciudad de Cuenca
         // 3. Crear una persona del Barrio Centenario de la ciuda de Guayaquil
+         Ciudad ciudad1 = new Ciudad("Loja");
+        Ciudad ciudad2 = new Ciudad("Zamora");
+        Ciudad ciudad3= new Ciudad("Cuenca");
+        Barrio barrio1 = new Barrio("San Sebastián", ciudad1);
+        Barrio barrio2 = new Barrio("Sucre", ciudad2);
+        Barrio barrio3=new Barrio("Platiado",ciudad3);
+        Persona persona1 = new Persona("René Elizalde", "1100909909", barrio1);
+        Persona persona2 = new Persona("Omer Benitez", "19500909909", barrio2);
+        Persona persona3 = new Persona("Luis Sarango", "1750909909", barrio3);
+        System.out.println("------------------------------------------");
+        System.out.printf("El barrio de la persona %s es: %s\n Y pertenece a la "
+                + "ciudad: %s\n",
+                persona1.obtenerNombre(),
+                persona1.obtenerBarrio().obtenerNombre(),
+                persona1.obtenerBarrio().obtenerCiudad().obtenerNombre());
+        System.out.println("------------------------------------------");
         
         
+        System.out.printf("El barrio de la persona %s es: %s\n Y pertenece a la "
+                + "ciudad: %s\n",
+                persona2.obtenerNombre(),
+                persona2.obtenerBarrio().obtenerNombre(),
+                persona2.obtenerBarrio().obtenerCiudad().obtenerNombre());
+        System.out.println("------------------------------------------");
+        
+        
+        barrio2.establecerCiudad(ciudad2);
+        System.out.printf("El barrio de la persona %s es: %s\n Y pertenece a la "
+                + "ciudad: %s\n",
+                persona3.obtenerNombre(),
+                persona3.obtenerBarrio().obtenerNombre(),
+                persona3.obtenerBarrio().obtenerCiudad().obtenerNombre());
     }
 }
